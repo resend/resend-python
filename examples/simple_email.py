5deb4f9872eb983e06ff135d08db3a8131982601
Derich Pacheco <d.erich@hotmail.com>
2023-06-13 21:41:58 -0300

    doc: update simple email example

--- examples/simple_email.py
+++ examples/simple_email.py
@@ -7,13 +7,13 @@ if not os.environ["RESEND_API_KEY"]:
 
 
 params = {
-    "from": "r@email.io",
-    "to": ["to@gmail.com"],
+    "from": "onboarding@resend.dev",
+    "to": ["delivered@resend.dev"],
     "subject": "hi",
     "html": "<strong>hello, world!</strong>",
     "reply_to": "to@gmail.com",
-    "bcc": "to@gmail.com",
-    "cc": ["to@gmail.com"],
+    "bcc": "delivered@resend.dev",
+    "cc": ["delivered@resend.dev"],
     "tags": [
         {"name": "tag1", "value": "tagvalue1"},
         {"name": "tag2", "value": "tagvalue2"},
