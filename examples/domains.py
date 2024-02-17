4209aff1434147da5b2ac10bfa88d77197c67172
Derich Pacheco <carlosderich@gmail.com>
2024-02-17 15:23:49 -0300

    feat: Impl Domains#update (#67)

--- examples/domains.py
+++ examples/domains.py
@@ -16,6 +16,15 @@ print(domain)
 retrieved = resend.Domains.get(domain_id=domain["id"])
 print(retrieved)
 
+update_params = {
+    "id": domain["id"],
+    "open_tracking": True,
+    "click_tracking": True,
+}
+
+updated = resend.Domains.update(update_params)
+print(updated)
+
 domains = resend.Domains.list()
 print(domains)
 
--- tests/domains_test.py
+++ tests/domains_test.py
@@ -178,3 +178,32 @@ class TestResendDomains(unittest.TestCase):
         )
         assert email is None
         patcher.stop()
+
+    def test_domains_update(self):
+        resend.api_key = "re_123"
+
+        patcher = patch("resend.Request.make_request")
+        mock = patcher.start()
+        mock.status_code = 200
+        m = MagicMock()
+        m.status_code = 200
+
+        def mock_json():
+            return {
+                "object": "domain",
+                "id": "479e3145-dd38-476b-932c-529ceb705947",
+            }
+
+        m.json = mock_json
+        mock.return_value = m
+
+        params = {
+            "id": "479e3145-dd38-476b-932c-529ceb705947",
+            "open_tracking": True,
+            "click_tracking": True,
+        }
+        contact = resend.Domains.update(params)
+        assert contact["id"] == "479e3145-dd38-476b-932c-529ceb705947"
+        assert contact["object"] == "domain"
+
+        patcher.stop()
