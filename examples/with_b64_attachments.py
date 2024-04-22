a4b32b0cf6aa2ea07ff650294f13a6e1a19bbe2e
Derich Pacheco <carlosderich@gmail.com>
2024-04-21 23:04:04 -0300

    examples: fix other examples

--- examples/with_b64_attachments.py
+++ examples/with_b64_attachments.py
@@ -6,20 +6,28 @@ import resend
 if not os.environ["RESEND_API_KEY"]:
     raise EnvironmentError("RESEND_API_KEY is missing")
 
+# Read file
 f = open(
     os.path.join(os.path.dirname(__file__), "../resources/invoice.pdf"), "rb"
 ).read()
 
-b64 = base64.b64encode(f)
-b64_str = b64.decode("utf-8")
+# Read file bytes as a base64 string
+b64: bytes = base64.b64encode(f)
+b64_str: str = b64.decode("utf-8")
 
-params = {
-    "from": "onboarding@resend.dev",
+# Create attachment object from base64 string
+b64_attachment: resend.Attachment = {"filename": "invoice.pdf", "content": b64_str}
+
+# Email params
+params: resend.Emails.SendParams = {
+    "sender": "onboarding@resend.dev",
     "to": ["delivered@resend.dev"],
     "subject": "hello with base64 attachments",
     "html": "<strong>hello, world!</strong>",
-    "attachments": [{"filename": "invoice.pdf", "content": b64_str}],
+    "attachments": [b64_attachment],
 }
 
+# Send email
 email = resend.Emails.send(params)
-print(email)
+print("Email sent with base64 string attachment")
+print(f"Email ID: {email.id}")
