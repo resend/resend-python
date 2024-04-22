42db6eb4fe98da8ef82ec85dfe680d27f1dd0fe4
Derich Pacheco <carlosderich@gmail.com>
2024-04-21 23:30:24 -0300

    fix: exceptions changes

--- tests/exceptions_test.py
+++ tests/exceptions_test.py
@@ -3,6 +3,7 @@ import unittest
 import pytest
 
 from resend.exceptions import (MissingApiKeyError, ResendError,
+                               ValidationError,
                                raise_for_code_and_type)
 
 
@@ -16,3 +17,13 @@ class TestResendError(unittest.TestCase):
         with pytest.raises(MissingApiKeyError) as e:
             raise_for_code_and_type(401, "missing_api_key", "err")
         assert e.type is MissingApiKeyError
+
+    def test_validation_error_from_422(self):
+        with pytest.raises(ValidationError) as e:
+            raise_for_code_and_type(422, "validation_error", "err")
+        assert e.type is ValidationError
+
+    def test_validation_error_from_400(self):
+        with pytest.raises(ValidationError) as e:
+            raise_for_code_and_type(400, "validation_error", "err")
+        assert e.type is ValidationError
