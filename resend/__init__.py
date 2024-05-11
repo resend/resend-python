7f6323eb2b0981785566b2730bba35a1176e586a
Derich Pacheco <carlosderich@gmail.com>
2024-05-11 12:55:44 -0300

    feat: expose Email and ApiKey types

--- resend/__init__.py
+++ resend/__init__.py
@@ -1,12 +1,14 @@
 import os
 
 from .api_keys._api_keys import ApiKeys
+from .api_keys._api_keys import ApiKey
 from .audiences._audiences import Audiences
 from .contacts._contacts import Contacts
 from .domains._domains import Domains
 from .emails._attachment import Attachment
 from .emails._batch import Batch
 from .emails._emails import Emails
+from .emails._emails import Email
 from .emails._tag import Tag
 from .request import Request
 from .version import get_version
@@ -28,6 +30,8 @@ __all__ = [
     "Audiences",
     "Contacts",
     # Types
+    "ApiKey",
+    "Email",
     "Attachment",
     "Tag",
 ]
