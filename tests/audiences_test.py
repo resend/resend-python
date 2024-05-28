a30a71598a33d73eaa683f4b7b5f8945e566f0ab
Pedro Impulcetto - Indeed <pedro.impulcetto@gmail.com>
2024-05-27 22:23:54 -0300

    feat: removing duplicate and unnecessary code to patcher and setup mock

--- tests/audiences_test.py
+++ tests/audiences_test.py
@@ -1,31 +1,18 @@
-import unittest
-from typing import Any, Dict, List
-from unittest.mock import MagicMock, patch
-
 import resend
+from tests.conftest import ResendBaseTest
 
 # flake8: noqa
 
 
-class TestResendAudiences(unittest.TestCase):
+class TestResendAudiences(ResendBaseTest):
     def test_audiences_create(self) -> None:
-        resend.api_key = "re_123"
-
-        patcher = patch("resend.Request.make_request")
-        mock = patcher.start()
-        mock.status_code = 200
-        m = MagicMock()
-        m.status_code = 200
-
-        def mock_json() -> Dict[Any, Any]:
-            return {
+        self.set_mock_json(
+            {
                 "object": "audience",
                 "id": "78261eea-8f8b-4381-83c6-79fa7120f1cf",
                 "name": "Registered Users",
             }
-
-        m.json = mock_json
-        mock.return_value = m
+        )
 
         params: resend.Audiences.CreateParams = {
             "name": "Python SDK Audience",
@@ -34,70 +21,37 @@ class TestResendAudiences(unittest.TestCase):
         assert audience["id"] == "78261eea-8f8b-4381-83c6-79fa7120f1cf"
         assert audience["name"] == "Registered Users"
 
-        patcher.stop()
-
     def test_audiences_get(self) -> None:
-        resend.api_key = "re_123"
-
-        patcher = patch("resend.Request.make_request")
-        mock = patcher.start()
-        mock.status_code = 200
-        m = MagicMock()
-        m.status_code = 200
-
-        def mock_json() -> Dict[Any, Any]:
-            return {
+        self.set_mock_json(
+            {
                 "object": "audience",
                 "id": "78261eea-8f8b-4381-83c6-79fa7120f1cf",
                 "name": "Registered Users",
                 "created_at": "2023-10-06T22:59:55.977Z",
             }
-
-        m.json = mock_json
-        mock.return_value = m
+        )
 
         audience = resend.Audiences.get(id="78261eea-8f8b-4381-83c6-79fa7120f1cf")
         assert audience["id"] == "78261eea-8f8b-4381-83c6-79fa7120f1cf"
         assert audience["name"] == "Registered Users"
         assert audience["created_at"] == "2023-10-06T22:59:55.977Z"
-        patcher.stop()
 
     def test_audiences_remove(self) -> None:
-        resend.api_key = "re_123"
-
-        patcher = patch("resend.Request.make_request")
-        mock = patcher.start()
-        mock.status_code = 200
-        m = MagicMock()
-        m.status_code = 200
-
-        def mock_json() -> Dict[Any, Any]:
-            return {
+        self.set_mock_json(
+            {
                 "object": "audience",
                 "id": "78261eea-8f8b-4381-83c6-79fa7120f1cf",
                 "deleted": True,
             }
-
-        m.json = mock_json
-        mock.return_value = m
+        )
 
         rmed = resend.Audiences.remove("78261eea-8f8b-4381-83c6-79fa7120f1cf")
         assert rmed["id"] == "78261eea-8f8b-4381-83c6-79fa7120f1cf"
         assert rmed["deleted"] is True
 
-        patcher.stop()
-
     def test_audiences_list(self) -> None:
-        resend.api_key = "re_123"
-
-        patcher = patch("resend.Request.make_request")
-        mock = patcher.start()
-        mock.status_code = 200
-        m = MagicMock()
-        m.status_code = 200
-
-        def mock_json() -> Dict[Any, Any]:
-            return {
+        self.set_mock_json(
+            {
                 "object": "list",
                 "data": [
                     {
@@ -107,11 +61,8 @@ class TestResendAudiences(unittest.TestCase):
                     }
                 ],
             }
-
-        m.json = mock_json
-        mock.return_value = m
+        )
 
         audiences: resend.Audiences.ListResponse = resend.Audiences.list()
         assert audiences["data"][0]["id"] == "78261eea-8f8b-4381-83c6-79fa7120f1cf"
         assert audiences["data"][0]["name"] == "Registered Users"
-        patcher.stop()
--- tests/batch_emails_test.py
+++ tests/batch_emails_test.py
@@ -1,32 +1,21 @@
-import unittest
-from typing import Any, Dict, List
-from unittest.mock import MagicMock, patch
+from typing import List
 
 import resend
+from tests.conftest import ResendBaseTest
 
 # flake8: noqa
 
 
-class TestResendBatchSend(unittest.TestCase):
+class TestResendBatchSend(ResendBaseTest):
     def test_batch_email_send(self) -> None:
-        resend.api_key = "re_123"
-
-        patcher = patch("resend.Request.make_request")
-        mock = patcher.start()
-        mock.status_code = 200
-        m = MagicMock()
-        m.status_code = 200
-
-        def mock_json() -> Dict[Any, Any]:
-            return {
+        self.set_mock_json(
+            {
                 "data": [
                     {"id": "ae2014de-c168-4c61-8267-70d2662a1ce1"},
                     {"id": "faccb7a5-8a28-4e9a-ac64-8da1cc3bc1cb"},
                 ]
             }
-
-        m.json = mock_json
-        mock.return_value = m
+        )
 
         params: List[resend.Emails.SendParams] = [
             {
@@ -42,8 +31,8 @@ class TestResendBatchSend(unittest.TestCase):
                 "html": "<strong>hello, world!</strong>",
             },
         ]
+
         emails: resend.Batch.SendResponse = resend.Batch.send(params)
         assert len(emails["data"]) == 2
         assert emails["data"][0]["id"] == "ae2014de-c168-4c61-8267-70d2662a1ce1"
         assert emails["data"][1]["id"] == "faccb7a5-8a28-4e9a-ac64-8da1cc3bc1cb"
-        patcher.stop()
