7698b1e69e693bb5c59bac4b54da82614348dfa5
Derich Pacheco <carlosderich@gmail.com>
2025-05-12 20:37:57 -0300

    feat: Support for custom_return_path (#143)

--- examples/domains.py
+++ examples/domains.py
@@ -10,6 +10,7 @@ if not os.environ["RESEND_API_KEY"]:
 create_params: resend.Domains.CreateParams = {
     "name": "example.com",
     "region": "us-east-1",
+    "custom_return_path": "outbound",
 }
 domain: resend.Domain = resend.Domains.create(params=create_params)
 print(domain)
