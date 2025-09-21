d3a8c70fe58a6b6e09070cff3aca7a1c1c2c6846
Derich Pacheco <carlosderich@gmail.com>
2025-09-21 17:03:43 -0300

    feat: audiencest list method pagination support

--- examples/audiences.py
+++ examples/audiences.py
@@ -21,6 +21,19 @@ print("Retrieved audience: ", aud)
 
 audiences: resend.Audiences.ListResponse = resend.Audiences.list()
 print("List of audiences:", [a["id"] for a in audiences["data"]])
+print(f"Has more audiences: {audiences['has_more']}")
+
+print("\n--- Using pagination parameters ---")
+if audiences["data"]:
+    paginated_params: resend.Audiences.ListParams = {
+        "limit": 5,
+        "after": audiences["data"][0]["id"]
+    }
+    paginated_audiences: resend.Audiences.ListResponse = resend.Audiences.list(params=paginated_params)
+    print(f"Retrieved {len(paginated_audiences['data'])} audiences with pagination")
+    print(f"Has more audiences: {paginated_audiences['has_more']}")
+else:
+    print("No audiences available for pagination example")
 
 rmed: resend.Audiences.RemoveAudienceResponse = resend.Audiences.remove(
     id=audience["id"]
