7739f8c2452cf96ee4b40aa61ba70aafb5391671
Derich Pacheco <carlosderich@gmail.com>
2025-10-21 00:51:18 -0300

    fix: use typing on variables

--- examples/templates.py
+++ examples/templates.py
@@ -1,28 +1,32 @@
 """Example usage of the Templates API."""
 
 import os
+from typing import List
 
 import resend
 
 if not os.environ["RESEND_API_KEY"]:
     raise EnvironmentError("RESEND_API_KEY is missing")
 
+# Define template variables with explicit typing
+variables: List[resend.Variable] = [
+    {
+        "key": "NAME",
+        "type": "string",
+        "fallback_value": "user",
+    },
+    {
+        "key": "AGE",
+        "type": "number",
+        "fallback_value": 25,
+    },
+]
+
 # Create a new template with variables
 create_params: resend.Templates.CreateParams = {
     "name": "welcome-email",
     "html": "<strong>Hey, {{{NAME}}}, you are {{{AGE}}} years old.</strong>",
-    "variables": [
-        {
-            "key": "NAME",
-            "type": "string",
-            "fallback_value": "user",
-        },
-        {
-            "key": "AGE",
-            "type": "number",
-            "fallback_value": 25,
-        },
-    ],
+    "variables": variables,
 }
 
 template: resend.Templates.CreateResponse = resend.Templates.create(create_params)
