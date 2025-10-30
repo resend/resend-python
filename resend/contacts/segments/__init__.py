532ffc7acccdd8988af52e6ca1790927ba18a738
Derich Pacheco <carlosderich@gmail.com>
2025-10-30 18:31:30 -0300

    fix: rm uncessary exports

--- resend/contacts/segments/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._contact_segment import ContactSegment
-from ._contact_segments import ContactSegments
-
-__all__ = ["ContactSegment", "ContactSegments"]
--- resend/segments/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._segment import Segment
-from ._segments import Segments
-
-__all__ = ["Segment", "Segments"]
