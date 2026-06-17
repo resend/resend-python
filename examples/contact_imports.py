d0ce8d6c0d205221d655a13ca453021054d42adb
drish <carlosderich@gmail.com>
2026-06-17 11:49:04 -0300

    feat: add contact imports example

---
+++ examples/contact_imports.py
@@ -0,0 +1,43 @@
+import os
+
+import resend
+
+if not os.environ["RESEND_API_KEY"]:
+    raise EnvironmentError("RESEND_API_KEY is missing")
+
+file_content = b"Email,First Name,Last Name,Plan\nsteve@example.com,Steve,Wozniak,pro"
+
+create_params: resend.ContactImports.CreateParams = {
+    "file": file_content,
+    "column_map": {
+        "email": "Email",
+        "first_name": "First Name",
+        "last_name": "Last Name",
+        "properties": {
+            "plan": {
+                "column": "Plan",
+                "type": "string",
+            },
+        },
+    },
+    "on_conflict": "upsert",
+    "segments": ["60a2ac5e-0774-456e-817d-ebf40f6dba31"],
+}
+
+import_response: resend.ContactImports.CreateContactImportResponse = (
+    resend.Contacts.Imports.create(create_params)
+)
+print("Created contact import with ID: {}".format(import_response["id"]))
+print(import_response)
+
+contact_import: resend.ContactImport = resend.Contacts.Imports.get(import_response["id"])
+print("Retrieved contact import")
+print(contact_import)
+
+list_response: resend.ContactImports.ListContactImportsResponse = (
+    resend.Contacts.Imports.list()
+)
+print(f"Found {len(list_response['data'])} imports")
+print(f"Has more: {list_response['has_more']}")
+for item in list_response["data"]:
+    print(item)
